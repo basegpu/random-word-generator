182bd674c1e03bbcc31ffe25dee929494f034217
Samuel Peter <sapeter@ethz.ch>
2021-11-09 21:34:39 +0100

    add tests

--- test/word-generator-tests.py
+++ test/word-generator-tests.py
@@ -1,9 +1,14 @@
 import pytest
-from app import *
+from syllable_shaker import *
 
 def test_new_generator_fail():
     with pytest.raises(TypeError):
         WordGenerator()
 
+def test_new_generator_empty():
+    with pytest.raises(Exception):
+        WordGenerator([])
+
 def test_new_generator_list():
-    gen = WordGenerator([])
+    gen = WordGenerator(['a'])
+    assert gen is not None
